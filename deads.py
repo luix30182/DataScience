71033eb213b5641c5c1a20b9dcded035afa485dd
Mario <luix30lp@gmail.com>
2019-03-28 07:26:25 -0600

    deads finish

---
+++ deads.py
@@ -0,0 +1,36 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_rows',1000)
+pd.set_option('display.max_columns',1000)
+pd.set_option('display.max_colwidth',1000)
+pd.set_option('display.width',1000)
+
+data = pd.read_csv(sys.argv[1], encoding='latin-1')
+word = sys.argv[2]
+entityName = pd.read_csv('DataScienceDataSets\\defunciones2017\\catalogos\\decateml.csv',encoding='latin-1')
+entityCVE = pd.read_csv('DataScienceDataSets\\defunciones2017\\conjunto_de_datos\\conjunto_de_datos_defunciones_generales_2017.CSV', encoding='latin-1')
+
+entityCVE = entityCVE.drop(columns=['tloc_resid','mun_regis','ent_resid','mun_resid','loc_resid','ent_ocurr','par_agre','ent_ocules','mun_ocules','loc_ocules','razon_m','dis_re_oax','mun_ocurr','tloc_ocurr','loc_ocurr','complicaro','dia_cert','mes_cert','anio_cert','maternas','lengua','sexo','edad','dia_ocurr','mes_ocurr','anio_ocur','grupo','lista1','gr_lismex','vio_fami','area_ur','edad_agru','cond_act','derechohab','embarazo','rel_emba','horas','minutos','capitulo','lugar_ocur','necropsia','asist_medi','sitio_ocur','cond_cert','dia_regis','mes_regis','anio_regis','dia_nacim','ocupacion','escolarida','edo_civil','presunto','ocurr_trab','lista_mex','mes_nacim','anio_nacim','nacionalid'])
+
+entityName = entityName.drop(columns=['cve_mun','cve_loc'])
+entityName = entityName.groupby('cve_ent').first()
+entityName = entityName[:-5]
+
+entityCVE.rename(columns={'ent_regis': 'id'}, inplace=True)
+entityName.index.names = ['id']
+
+fulldata = pd.merge(entityCVE,entityName, on='id')
+fulldata.rename(columns={'causa_def':'CVE'}, inplace=True)
+fulldata2 = pd.merge(fulldata,data, on='CVE')
+fulldata2['DESCRIP'] = fulldata2['DESCRIP'].str.lower()
+
+filterRows = fulldata2[fulldata2['DESCRIP'].str.contains(word)]
+filtered = filterRows.groupby(['nom_loc','CVE']).first()
+filteredCounter = filterRows.groupby(['nom_loc'])['DESCRIP'].count()
+print(filteredCounter)
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', 10000):
+    print(filtered)
--- entriopia.py
+++ entriopia.py
@@ -2,18 +2,26 @@ import sys
 import pandas as pd
 import numpy as np
 
-def getShannon(n):
-  m = 1/n
-  l = np.log(n)
-  print('El valor de n es:',n) 
-  print('El valor del log es:',l)
-  return n*m*l
+def getShannon(d,n):
+  s = 0
+  for x in d.values():
+    f = x/n
+    l = np.log2(f)
+    r = f * l
+    s += r
+  return s*-1
 
 data = pd.read_csv(sys.argv[1])
+col = sys.argv[2]
 
-data = data.drop(columns=['YEAR','MONTH','DAY','DAY_OF_WEEK','FLIGHT_NUMBER','TAIL_NUMBER','TAXI_OUT','WHEELS_OFF','DESTINATION_AIRPORT','SCHEDULED_DEPARTURE','DEPARTURE_DELAY','SCHEDULED_TIME','DISTANCE','ELAPSED_TIME','WHEELS_ON','TAXI_IN','SCHEDULED_ARRIVAL','ARRIVAL_TIME','AIR_TIME','DIVERTED','CANCELLED','CANCELLATION_REASON','WEATHER_DELAY','LATE_AIRCRAFT_DELAY','AIR_SYSTEM_DELAY','SECURITY_DELAY','DEPARTURE_TIME'])
+endata = data[col].values
 
-endata = data['ARRIVAL_DELAY'].values
-# print(endata)
+datat = {}
 
-print(getShannon(len(endata)))
+for x in endata:
+  try:
+    datat[x] += 1
+  except:
+    datat[x] = 1
+
+print(getShannon(datat,len(endata)))
